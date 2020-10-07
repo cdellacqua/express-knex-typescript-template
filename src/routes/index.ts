9629a3cf2ca1d4262ed80b26d213f31ceb6635fc
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-10-07 20:32:08 +0200

    add explanatory TODO

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,8 @@ export default r;
 r.use('/api', apiRoutes);
 r.use('/', ssrRoutes);
 
+// TODO: for development purposes express will serve static files from the www directory, in a production environment
+// this task should be performed by a reverse proxy to improve performance
 if (process.env.NODE_ENV === 'development') {
 	r.use('/', expressStatic(path.join(__dirname, '..', '..', 'www')));
 }
