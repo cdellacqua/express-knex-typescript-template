d333c0cf63b050c68791bce7ebfe1cb2160418ae
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-10-22 23:14:15 +0200

    add asyncWrapper in middleware

--- src/routes/api/authenticated/_middleware.ts
+++ src/routes/api/authenticated/_middleware.ts
@@ -1,44 +1,38 @@
 import { SerializableError } from '@cdellacqua/serializable-error';
-import {
-	NextFunction, Request, Response,
-} from 'express';
 import jwt from 'jsonwebtoken';
 import { container } from 'tsyringe';
+import { asyncWrapper } from '@cdellacqua/express-async-wrapper';
 import { HttpError } from '../../../http/error';
 import config from '../../../config';
 import { UserService } from '../../../services/user';
 
 // TODO: customize your authorization logic
-export default (req: Request, res: Response, next: NextFunction): void => {
+const middleware = asyncWrapper(async (req, res, next) => {
 	if (!req.headers.authorization) {
-		next(new HttpError(401, 'unauthorized', undefined, false));
-		return;
+		throw new HttpError(401, 'unauthorized', undefined, false);
 	}
+	let decoded;
 	try {
-		const decoded = jwt.verify(req.headers.authorization.substring('Bearer '.length), config.secret) as unknown as {sub: string, iat: number};
-		if (!decoded) {
-			next(new HttpError(401, 'unauthorized', new SerializableError('jwt decode returned falsy value'), false));
-			return;
-		}
-		const userService = container.resolve(UserService);
-		userService.find(decoded.sub)
-			.then((user) => {
-				if (!user) {
-					next(new HttpError(401, 'unauthorized', new SerializableError('jwt refers to missing user'), false));
-					return;
-				}
-				if (!user.enabled) {
-					next(new HttpError(401, 'unauthorized', new SerializableError('jwt refers to disabled user', false)));
-					return;
-				}
-				if (user.minJwtIat.getTime() > decoded.iat * 1000) {
-					next(new HttpError(401, 'unauthorized', new SerializableError('jwt iat is less than the min required for the specified user'), false));
-					return;
-				}
-				res.locals.user = user;
-				next();
-			}, (err) => next(err));
+		decoded = jwt.verify(req.headers.authorization.substring('Bearer '.length), config.secret) as unknown as {sub: string, iat: number};
 	} catch (err) {
-		next(new HttpError(401, 'unauthorized', err));
+		throw new HttpError(401, 'unauthorized', err);
 	}
-};
+	if (!decoded) {
+		throw new HttpError(401, 'unauthorized', new SerializableError('jwt decode returned falsy value'), false);
+	}
+	const userService = container.resolve(UserService);
+	const user = await userService.find(decoded.sub);
+	if (!user) {
+		throw new HttpError(401, 'unauthorized', new SerializableError('jwt refers to missing user'), false);
+	}
+	if (!user.enabled) {
+		throw new HttpError(401, 'unauthorized', new SerializableError('jwt refers to disabled user'), false);
+	}
+	if (user.minJwtIat.getTime() > decoded.iat * 1000) {
+		throw new HttpError(401, 'unauthorized', new SerializableError('jwt iat is less than the min required for the specified user'), false);
+	}
+	res.locals.user = user;
+	next();
+});
+
+export default middleware;
