dd1e0ec9d377e68e0905b4f6dc6dffbddbae2dd0
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2020-10-25 15:20:10 +0100

    add TODO

--- src/routes/api/authenticated/goodbye.ts
+++ src/routes/api/authenticated/goodbye.ts
@@ -3,4 +3,5 @@ import { Router } from 'express';
 const r: Router = Router();
 export default r;
 
+// TODO: remove this example
 r.get('/', (_, res) => res.send(`goodbye ${res.locals.user.email}`));
