942c1cecf7ef4ad0f87523e5fa7eb21f80052620
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-01-09 15:43:26 +0100

    add streams support and reduce code duplication in db/utils

--- src/db/utils.ts
+++ src/db/utils.ts
@@ -2,11 +2,16 @@ import { transact } from '@cdellacqua/knex-transact';
 import { SerializableError } from '@cdellacqua/serializable-error';
 /* eslint-disable no-underscore-dangle */
 import { Transaction, QueryBuilder } from 'knex';
+import { Readable, Transform } from 'stream';
 import knex from '.';
 
 export function insertGetIds<T>(query: QueryBuilder): Promise<T[]> {
 	return query.returning<T[]>('id');
 }
+
+export type OrderByColumn = {column: string; order: 'asc'|'desc'};
+
+export type OrderByArray = OrderByColumn[];
 
 export async function insertGetId<T>(query: QueryBuilder): Promise<T> {
 	const ids = await insertGetIds<T>(query);
@@ -18,6 +23,16 @@ export async function insertGetId<T>(query: QueryBuilder): Promise<T> {
 
 export async function selectId<T>(query: QueryBuilder): Promise<T> {
 	return (await query.pluck<T[]>('id'))[0];
+}
+
+function rowToEntityTransformStream<TEntity>(rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>, trx?: Transaction) {
+	return new Transform({
+		objectMode: true,
+		transform(row, _, done) {
+			rowToEntity(row, trx)
+				.then((obj) => done(null, obj), done);
+		},
+	});
 }
 
 export function createMultiGenerator<TSave, TEntity>(
@@ -76,6 +91,23 @@ export function findOneGenerator<TFilter = Record<string, any> | string | number
 	};
 }
 
+function findFirstsQuery<TFilter = Record<string, any> | string | number>(
+	table: string,
+	columns: string[],
+	filters: TFilter[],
+	trx?: Transaction,
+) {
+	const query = trx?.queryBuilder() || knex.queryBuilder();
+	return filters.reduce<QueryBuilder>((_query, filter) => _query.unionAll(function findSingle() {
+		return this.table(table)
+			.where(
+				typeof filter === 'object' ? filter : { id: filter },
+			)
+			.limit(1)
+			.select(columns);
+	}, true), query);
+}
+
 export function findFirstsGenerator<TFilter = Record<string, any> | string | number, TEntity = object>(
 	table: string,
 	columns: string[],
@@ -83,15 +115,7 @@ export function findFirstsGenerator<TFilter = Record<string, any> | string | num
 ) {
 	return async (filters: TFilter[], trx?: Transaction): Promise<TEntity[]> => {
 		if (filters.length > 0) {
-			const query = trx?.queryBuilder() || knex.queryBuilder();
-			const rows: any[] = await filters.reduce<QueryBuilder>((_query, filter) => _query.unionAll(function findSingle() {
-				return this.table(table)
-					.where(
-						typeof filter === 'object' ? filter : { id: filter },
-					)
-					.limit(1)
-					.select(columns);
-			}, true), query);
+			const rows = await findFirstsQuery(table, columns, filters, trx);
 			if (rows.length !== filters.length) {
 				throw new SerializableError('unable to find all the rows');
 			}
@@ -101,25 +125,64 @@ export function findFirstsGenerator<TFilter = Record<string, any> | string | num
 	};
 }
 
+export function findFirstsStreamGenerator<TFilter = Record<string, any> | string | number, TEntity = object>(
+	table: string,
+	columns: string[],
+	rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>,
+) {
+	return (filters: TFilter[], trx?: Transaction): Readable => {
+		if (filters.length > 0) {
+			return findFirstsQuery(table, columns, filters, trx)
+				.pipe(rowToEntityTransformStream(rowToEntity, trx));
+		}
+		return Readable.from([], {
+			objectMode: true,
+		});
+	};
+}
+
+function findMultiQuery<TFilter = Record<string, any> | string | number, TEntity = object>(
+	table: string,
+	columns: string[],
+	filters: TFilter[], orderBy?: OrderByArray, trx?: Transaction,
+) {
+	const query = trx?.queryBuilder() || knex.queryBuilder();
+	return filters.reduce<QueryBuilder>((_query, filter) => _query.unionAll(function findSingle() {
+		return this.table(table)
+			.where(
+				typeof filter === 'object' ? filter : { id: filter },
+			)
+			.select(columns);
+	}, true), query).orderBy(orderBy || []);
+}
+
 export function findMultiGenerator<TFilter = Record<string, any> | string | number, TEntity = object>(
 	table: string,
 	columns: string[],
 	rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>,
 ) {
-	return async (filters: TFilter[], orderBy?: {column: string; order: 'asc'|'desc'}[], trx?: Transaction): Promise<TEntity[]> => {
+	return async (filters: TFilter[], orderBy?: OrderByArray, trx?: Transaction): Promise<TEntity[]> => {
 		if (filters.length > 0) {
-			const query = trx?.queryBuilder() || knex.queryBuilder();
-			const rows: any[] = await filters.reduce<QueryBuilder>((_query, filter) => _query.unionAll(function findSingle() {
-				return this.table(table)
-					.where(
-						typeof filter === 'object' ? filter : { id: filter },
-					)
-					.select(columns);
-			}, true), query).orderBy(orderBy || []);
-
+			const rows: any[] = await findMultiQuery(table, columns, filters, orderBy, trx);
 			return Promise.all(rows.map((row: any) => rowToEntity(row, trx)));
 		}
 		return [];
+	};
+}
+
+export function findMultiStreamGenerator<TFilter = Record<string, any> | string | number, TEntity = object>(
+	table: string,
+	columns: string[],
+	rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>,
+) {
+	return (filters: TFilter[], orderBy?: OrderByArray, trx?: Transaction): Readable => {
+		if (filters.length > 0) {
+			return findMultiQuery(table, columns, filters, orderBy, trx)
+				.pipe(rowToEntityTransformStream(rowToEntity, trx));
+		}
+		return Readable.from([], {
+			objectMode: true,
+		});
 	};
 }
 
@@ -128,7 +191,7 @@ export function findGroupedMultiGenerator<TFilter = Record<string, any> | string
 	columns: string[],
 	rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>,
 ) {
-	return async (filters: TFilter[], orderBy?: {column: string; order: 'asc'|'desc'}[], trx?: Transaction): Promise<TEntity[][]> => {
+	return async (filters: TFilter[], orderBy?: OrderByArray, trx?: Transaction): Promise<TEntity[][]> => {
 		if (filters.length > 0) {
 			const query = trx?.queryBuilder() || knex.queryBuilder();
 			const rows: any[] = await filters.reduce<QueryBuilder>((_query, filter, index) => _query.unionAll(function findSingle() {
@@ -161,22 +224,44 @@ export function findGroupedMultiGenerator<TFilter = Record<string, any> | string
 	};
 }
 
+function findAllQuery<TFilter = Record<string, any> | string | number>(
+	table: string,
+	columns: string[],
+	filter: TFilter,
+	orderBy?: OrderByArray,
+	trx?: Transaction,
+) {
+	const query = trx?.queryBuilder() || knex.queryBuilder();
+	return query.table(table)
+		.where(
+			typeof filter === 'object' ? filter : { id: filter },
+		)
+		.orderBy(orderBy ?? [])
+		.select(columns);
+}
+
 export function findAllGenerator<TFilter = Record<string, any> | string | number, TEntity = object>(
 	table: string,
 	columns: string[],
 	rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>,
 ) {
-	return async (filter: TFilter, orderBy?: {column: string; order: 'asc'|'desc'}[], trx?: Transaction): Promise<TEntity[]> => {
-		const query = trx?.queryBuilder() || knex.queryBuilder();
-		const rows: any[] = await query.table(table)
-			.where(
-				typeof filter === 'object' ? filter : { id: filter },
-			)
-			.orderBy(orderBy ?? [])
-			.select(columns);
-
+	return async (filter: TFilter, orderBy?: OrderByArray, trx?: Transaction): Promise<TEntity[]> => {
+		const rows: any[] = await findAllQuery(table, columns, filter, orderBy, trx);
 		return Promise.all(rows.map((row: any) => rowToEntity(row, trx)));
 	};
+}
+
+export function findAllStreamGenerator<TFilter = Record<string, any> | string | number, TEntity = object>(
+	table: string,
+	columns: string[],
+	rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>,
+) {
+	return (
+		filter: TFilter,
+		orderBy?: OrderByArray,
+		trx?: Transaction,
+	): Readable => findAllQuery(table, columns, filter, orderBy, trx)
+		.pipe(rowToEntityTransformStream(rowToEntity, trx));
 }
 
 export function fromQueryGenerator<TEntity = object>(
@@ -187,4 +272,12 @@ export function fromQueryGenerator<TEntity = object>(
 		const rows: any[] = await query.select(columns);
 		return Promise.all(rows.map((row: any) => rowToEntity(row, trx)));
 	};
+}
+
+export function fromQueryStreamGenerator<TEntity = object>(
+	columns: string[],
+	rowToEntity: (row: any, trx?: Transaction) => Promise<TEntity>,
+) {
+	return (query: QueryBuilder, trx?: Transaction): Readable => query.select(columns)
+		.pipe(rowToEntityTransformStream(rowToEntity, trx));
 }
