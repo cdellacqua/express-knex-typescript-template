438837ee1d3dd609bfc8d80d9767cda7e95c8147
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-01-14 22:38:10 +0100

    add process env variable check at startup

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,33 @@
-export default {
+import { SerializableError } from '@cdellacqua/serializable-error';
+
+const config = {
 	http: {
-		hostname: process.env.HOST || 'localhost',
-		port: Number(process.env.PORT || 3000),
+		hostname: process.env.HOST!,
+		port: Number(process.env.PORT),
 	},
 	authentication: {
-		tokenExpirationSeconds: Number(process.env.JWT_EXPIRATION_SECONDS || 8640000),
+		tokenExpirationSeconds: Number(process.env.JWT_EXPIRATION_SECONDS),
 	},
-	environment: process.env.NODE_ENV || 'development',
-	secret: process.env.SECRET || 'secret',
+	environment: process.env.NODE_ENV!,
+	secret: process.env.SECRET!,
 	log: {
-		level: process.env.LOG_LEVEL || 'http',
+		level: process.env.LOG_LEVEL!,
 	},
 	shutdown: {
 		interval: Number(process.env.SHUTDOWN_INTERVAL_SECONDS || 10),
 	},
 };
+
+function recursiveCheck(obj: Record<string, any>, path: string[] = []) {
+	Object.keys(obj).forEach((key) => {
+		if (obj[key] === undefined || Number.isNaN(obj[key])) {
+			throw new SerializableError(`missing env variable for config key "${[...path, key].join('.')}"`);
+		} else if (typeof obj[key] === 'object') {
+			recursiveCheck(obj[key], [...path, key]);
+		}
+	});
+}
+
+recursiveCheck(config);
+
+export default config;
