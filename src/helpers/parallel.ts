fa5c0c9455cfe56e96cf1cf0fe9fc55c403387d4
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-03-01 15:45:56 +0100

    improve parallel helper

--- src/helpers/parallel.ts
+++ src/helpers/parallel.ts
@@ -1,10 +1,14 @@
 import { chunk } from './collection';
 
-export async function startParallel(fns: (() => Promise<any>)[]): Promise<void> {
-	const chunks = chunk(fns, 10);
-	await chunks
-		.reduce(
-			(chain, fnsChunk) => chain.then(async () => { await Promise.all(fnsChunk); }),
-			Promise.resolve(),
-		);
+export async function awaitParallel(fns: (() => Promise<any>)[], concurrency?: number): Promise<void> {
+	if (!concurrency) {
+		await Promise.all(fns);
+	} else {
+		const chunks = chunk(fns, concurrency);
+		await chunks
+			.reduce(
+				(chain, fnsChunk) => chain.then(async () => { await Promise.all(fnsChunk); }),
+				Promise.resolve(),
+			);
+	}
 }
