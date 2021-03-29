6f71cf7e8e0c2f5198cd0f0672e8543df54b110d
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-03-29 15:29:59 +0200

    add more helper functions to collection.ts

--- src/helpers/collection.ts
+++ src/helpers/collection.ts
@@ -19,3 +19,31 @@ export function chunk<T>(arr: T[], chunkLength: number): T[][] {
 
 	return chunked;
 }
+
+export function chain<T>(arr: T[]): T[][] {
+	if (arr.length === 0) {
+		return [];
+	}
+	const pairs = new Array(arr.length - 1);
+	for (let i = 0; i < pairs.length; i++) {
+		pairs[i] = [arr[i], arr[i + 1]];
+	}
+	return pairs;
+}
+
+export function pluck<T>(arr: any[], prop: string): T[] {
+	return arr.map((item) => item[prop]);
+}
+
+export function partition<T, TKey extends string|number>(arr: T[], partFn: ((v: T, i: number, arr: T[]) => TKey)): { [k in TKey]?: T[] } {
+	return arr.reduce((part: { [k in TKey]?: T[] }, cur, i) => {
+		const key = partFn(cur, i, arr);
+
+		if (!part[key]) {
+			part[key] = [];
+		}
+		part[key]!.push(cur);
+
+		return part;
+	}, {} as { [k in TKey]?: T[] });
+}
