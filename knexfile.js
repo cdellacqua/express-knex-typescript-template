04e513bf681a7cd329d2160ef15faa2c6ec2f00b
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-05-07 21:42:28 +0200

    add staging to knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,7 @@ const config = {
 		user: process.env.DB_USER,
 		password: process.env.DB_PASS,
 	},
+	debug: ['development', 'test'].includes(process.env.NODE_ENV),
 	pool: {
 		min: 2,
 		max: 10,
@@ -22,16 +23,8 @@ const config = {
 };
 
 module.exports = {
-	development: {
-		...config,
-		debug: true,
-	},
-	test: {
-		...config,
-		debug: false,
-	},
-	production: {
-		...config,
-		debug: false,
-	},
+	development: config,
+	test: config,
+	staging: config,
+	production: config,
 };
